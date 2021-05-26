2d4ee644e0b47020639b55d36d108b7eefbedd86
Sergey Mychailichenko <4eptnk@gmail.com>
2021-05-26 21:37:25 +0300

    *
    правки

--- source/logic.js
+++ source/logic.js
@@ -1,30 +1,27 @@
 const colors = [
-  '#FFFFFF',
-  '#2196F3',
-  '#4CAF50',
-  '#FF9800',
-  '#009688',
-  '#795548',
+  "#FFFFFF",
+  "#2196F3",
+  "#4CAF50",
+  "#FF9800",
+  "#009688",
+  "#795548",
 ];
 
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-const random = randomIntegerFromInterval(0, 5);
-var randomColor = colors[random];
-
 const start = document.querySelector("[data-action='start']");
-start.addEventListener('click', e => {
-  start.setAttribute('disabled', '');
-  stop.removeAttribute('disabled');
+start.addEventListener("click", (e) => {
+  start.setAttribute("disabled", "");
+  stop.removeAttribute("disabled");
   startTimer();
 });
 
 const stop = document.querySelector("[data-action='stop']");
-stop.addEventListener('click', e => {
-  stop.setAttribute('disabled', '');
-  start.removeAttribute('disabled');
+stop.addEventListener("click", (e) => {
+  stop.setAttribute("disabled", "");
+  start.removeAttribute("disabled");
   stopTimer();
 });
 
@@ -33,12 +30,15 @@ function changeBackground(color) {
 }
 
 function startTimer() {
-  id = setInterval(function () {
-    color = colors[randomIntegerFromInterval(0, 5)];
-    changeBackground(color);
-  }, 1000);
+  if (typeof change === "undefined" || change === null) {
+    change = setInterval(function () {
+      color = colors[randomIntegerFromInterval(0, colors.length - 1)];
+      changeBackground(color);
+    }, 1000);
+  }
 }
 
 function stopTimer() {
-  clearInterval(id);
+  clearInterval(change);
+  change = null;
 }
